0489bbbc766d20e725925e438a3fcf1d9987e018
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-14 16:55:09 +0100

    Added option to create new articles and redirect to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,24 @@ class ArticlesController < ApplicationController
   def show
   end
 
+  def new
+    @new_article = Article.new
+  end
+
+  def create
+    @new_article = Article.new(form_params)
+    @new_article.save
+    redirect_to articles_path
+  end
+
   private
 
   def find_article
     @id = params[:id]
     @article = Article.find(@id)
+  end
+
+  def form_params
+    params.require(:article).permit(:title, :content)
   end
 end
