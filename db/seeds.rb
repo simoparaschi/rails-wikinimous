46818b31a63d9286ccacdc098a31b152ba7ecc75
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-14 16:23:57 +0100

    Added seeds Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::RuPaul.queen,
+    content: Faker::TvShows::RuPaul.quotes
+  )
+  article.save!
+end
