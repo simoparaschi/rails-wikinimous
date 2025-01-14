7b27eadae372194a9d67d8704da856a215fce9a7
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-14 16:48:01 +0100

    Added option to find and read an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
   def index
     @articles = Article.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_article
+    @id = params[:id]
+    @article = Article.find(@id)
   end
 end
